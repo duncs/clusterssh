bf60b7e47e209a22aa513d92a35e0f4afa7dff56
Duncan Ferguson <duncan_j_ferguson@yahoo.co.uk>
2009-05-05 21:38:12 +0100

    Add pod and rename method

--- clusterssh/lib/ClusterSSH/Config/Base.pm
+++ clusterssh/lib/ClusterSSH/Config/Base.pm
@@ -50,15 +50,82 @@ use base qw/ ClusterSSH::Base /;
         return $self;
     }
 
+    sub DESTROY {
+        my ($self) = @_;
+        delete $filename_for{ $self->id };
+        return;
+    }
+
     sub get_filename {
         my ($self) = @_;
         return $filename_for{ $self->id };
     }
 
-    sub _get_config_hash {
+    sub get_config_hash {
         my ($self) = @_;
         croak( $self->loc('This method should have been replaced') );
     }
 }
 
 1;
+
+__END__
+
+=pod
+
+=head1 
+
+ClusterSSH::Config::Base
+
+=head1 SYNOPSIS
+
+    use base qw/ ClusterSSH::Config::Base /;
+
+    # in object new method
+    sub new {
+        ( $class, $arg_ref ) = @_;
+        my $self = $class->SUPER::new($arg_ref);
+        return $self;
+    }
+
+=head1 DESCRIPTION
+
+Base object to provide some utility functions on configuration objects 
+that access files - should not be used directly.  See also L<ClusterSSH::Base>.
+
+=head1 METHODS
+
+These extra methods are provided on the object
+
+=over 4
+
+=item $obj = CLusterSSH::Config::Base->new({ filename => '/path/to/file' });
+
+Creates object.  In higher debug levels the args are printed out.  Does some
+basic checks to ensure the C<filename> exists.  Croaks on problems.
+
+=item $obj->get_filename();
+
+Returns filename object was created with.
+
+=item %config = %{ $obj->get_config_hash };
+
+Return a hash of configuration items from the given file.  Needs to be overidden
+by child objects.
+
+=back
+
+=head1 AUTHOR
+Duncan Ferguson (<duncan_j_ferguson (at) yahoo.co.uk>)
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright (c) 2009 Duncan Ferguson (<duncan_j_ferguson (at) yahoo.co.uk>). 
+All rights reserved
+
+This module is free software; you can redistribute it and/or
+modify it under the same terms as Perl itself.  See L<perlartistic>.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
