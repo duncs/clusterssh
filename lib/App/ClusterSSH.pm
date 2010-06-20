2fb80f3ee4adefc8d41745ebca6eb775dae8488e
Duncan Ferguson <duncan_j_ferguson@yahoo.co.uk>
2010-06-20 17:53:39 +0100

    Update version number

--- lib/App/ClusterSSH.pm
+++ lib/App/ClusterSSH.pm
@@ -3,7 +3,7 @@ package App::ClusterSSH;
 use 5.008.004;
 use warnings;
 use strict;
-use version; our $VERSION = version->new('4.00_04');
+use version; our $VERSION = version->new('4.00_05');
 
 use Carp;
 
