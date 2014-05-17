61a04ac3d298c5e78860e0995ea7236d3e75bee6
Duncan Ferguson <duncan_j_ferguson@yahoo.co.uk>
2014-05-17 17:17:43 +0100

    Release 4.02_04

--- lib/App/ClusterSSH.pm
+++ lib/App/ClusterSSH.pm
@@ -3,7 +3,7 @@ package App::ClusterSSH;
 use 5.008.004;
 use warnings;
 use strict;
-use version; our $VERSION = version->new('4.02_03');
+use version; our $VERSION = version->new('4.02_04');
 
 use Carp;
 
