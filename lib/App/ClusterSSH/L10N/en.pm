88d4946596e46ac2d3422e1b22495ed4c458ff83
Duncan Ferguson <duncan_j_ferguson@yahoo.co.uk>
2014-05-29 10:13:05 +0100

    More pod translations

--- lib/App/ClusterSSH/L10N/en.pm
+++ lib/App/ClusterSSH/L10N/en.pm
@@ -1,7 +1,22 @@
 package App::ClusterSSH::L10N::en;
 use base 'App::ClusterSSH::L10N';
 
-%Lexicon = ( '_AUTO' => 1, );
+%Lexicon = ( '_AUTO' => 1, 
+  '_DESCRIPTION' => q{The command opens an administration console and an xterm to all specified hosts.  Any text typed into the administration console is replicated to all windows.  All windows may also be typed into directly.
+
+This tool is intended for (but not limited to) cluster administration where the same configuration or commands must be run on each node within the cluster.  Performing these commands all at once via this tool ensures all nodes are kept in sync.
+
+Connections are opened via ssh, so a correctly installed and configured ssh installation is required.  If, however, the program is called by "crsh" then the rsh protocol is used (and the communications channel is insecure), or by "ctel" then telnet is used, or by "ccon" then console is used.
+
+Extra caution should be taken when editing system files such as /etc/inet/hosts as lines may not necessarily be in the same order.  Assuming line 5 is the same across all servers and modifying that is dangerous.  It's better to search for the specific line to be changed and double-check before changes are committed.},
+
+    '_FURTHER_NOTES' => q{Further Notes},
+    '_OPTIONS' => q{Some of these options may also be defined within the configuration file. 
+
+Default options are shown as appropriate.
+    },
+
+);
 
 1;
 
