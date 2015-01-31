c53eedaf1c237e4cd8d901d1c00be3fb7c8635fe
Duncan Ferguson <duncan_j_ferguson@yahoo.co.uk>
2015-01-31 10:47:30 +0000

    Update version and changes file with patch from Andrew Stevenson

--- lib/App/ClusterSSH.pm
+++ lib/App/ClusterSSH.pm
@@ -3,7 +3,7 @@ package App::ClusterSSH;
 use 5.008.004;
 use warnings;
 use strict;
-use version; our $VERSION = version->new('4.03_05');
+use version; our $VERSION = version->new('4.03_06');
 
 use Carp qw/cluck/;
 
