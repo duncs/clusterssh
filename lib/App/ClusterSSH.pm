3fcc001cca02173b1d3be107d53b4c505b238593
Duncan Ferguson <duncan_j_ferguson@yahoo.co.uk>
2015-11-09 13:39:16 +0000

    Bump version number to 4.04_01

--- lib/App/ClusterSSH.pm
+++ lib/App/ClusterSSH.pm
@@ -3,7 +3,7 @@ package App::ClusterSSH;
 use 5.008.004;
 use warnings;
 use strict;
-use version; our $VERSION = version->new('4.04');
+use version; our $VERSION = version->new('4.04_01');
 
 use Carp qw/cluck :DEFAULT/;
 
