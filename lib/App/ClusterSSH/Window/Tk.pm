3d571b28015a4f68f0a087bffd5147824e178a84
Duncan Ferguson <duncan_j_ferguson@yahoo.co.uk>
2016-04-08 20:51:18 +0100

    Start reworking Tk into a module

---
+++ lib/App/ClusterSSH/Window/Tk.pm
@@ -0,0 +1,16 @@
+package App::ClusterSSH::Window::Tk;
+use strict;
+use warnings;
+
+use version;
+our $VERSION = version->new('0.01');
+
+use Carp;
+use Tk 800.022;
+
+use base qw/ App::ClusterSSH::Base /;
+
+
+
+
+1;
