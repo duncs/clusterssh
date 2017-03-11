a3e8e221e749938cb949920800b32b7ec6b31b39
Duncan Ferguson <duncan_j_ferguson@yahoo.co.uk>
2017-03-11 08:40:10 +0000

    Update version

--- lib/App/ClusterSSH.pm
+++ lib/App/ClusterSSH.pm
@@ -3,7 +3,7 @@ package App::ClusterSSH;
 use 5.008.004;
 use warnings;
 use strict;
-use version; our $VERSION = version->new('4.09');
+use version; our $VERSION = version->new('4.10');
 
 use Carp qw/cluck :DEFAULT/;
 
