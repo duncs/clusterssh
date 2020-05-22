duncs/clusterssh

commit 82f88450d0db5184906d75f3fb47eb0fb6e8415c

Add in 'command_pre' and 'command_post' configs
This allows for running commands before and after the comms method in
the command pipeline, such as setting up python virtual environments
and piping output through other commands

lib/App/ClusterSSH/Helper.pm
@@ -52,6 +52,8 @@ sub script {
         }
     }
 
+    my $command_pre    = $config->{command_pre}  || q{};
+    my $command_post   = $config->{command_post} || q{};
     my $comms          = $config->{ $config->{comms} };
     my $comms_args     = $config->{ $config->{comms} . '_args' };
     my $config_command = $config->{command};
@@ -69,7 +71,7 @@ sub script {
            my \$user=shift;
            my \$port=shift;
            my \$mstr=shift;
-           my \$command="$comms $comms_args ";
+           my \$command="$command_pre $comms $comms_args";
            open(PIPE, ">", \$pipe) or die("Failed to open pipe: \$!\\n");
            print PIPE "\$\$:\$ENV{WINDOWID}" 
                or die("Failed to write to pipe: $!\\n");
@@ -105,6 +107,7 @@ sub script {
            if("$config_command") {
             \$command .= " \\\"$config_command\\\"";
            }
+           \$command .= "$command_post";
            \$command .= " ; $postcommand";
            # provide some info for debugging purposes
            warn("Running: \$command\\n");

README
@@ -346,6 +346,21 @@ FILES
             Enable or disable alternative algorithm for calculating terminal
             positioning.
 
+        command_pre =
+        command_post =
+            Add extra commands around the communication method. For example:
+
+            command_pre= . $HOME/virtualenvs/default/bin/active ;
+            command_post= | ct
+
+            would allow for using Python virtual envronments and then piping
+            all shell output through "chromaterm" for syntax highlighting.
+            Note: you must use appropriate command separators/terminators to
+            keep the meaning of the command pipline (such as ";" and "|"
+            between commands).
+
+            These are not put through macro parsing.
+
         comms = ssh
             Sets the default communication method (initially taken from the
             name of the program, but can be overridden here).

lib/App/ClusterSSH/Config.pm
@@ -98,6 +98,8 @@ my %default_config = (
     history_height => 10,
 
     command             => q{},
+    command_pre         => q{},
+    command_post        => q{},
     hide_menu           => 0,
     max_host_menu_items => 30,
 

lib/App/ClusterSSH/Getopt.pm
@@ -700,6 +700,17 @@ would replace the <Alt-n> with the client's name in each window.}
         'Enable or disable alternative algorithm for calculating terminal positioning.',
     );
 
+    output '=item command_pre =';
+    output '=item command_post =';
+    output $self->loc(
+        q{Add extra commands around the communication method.  For example:
+
+command_pre= . $HOME/virtualenvs/default/bin/active ;
+command_post= | ct
+
+would allow for using Python virtual envronments and then piping all shell output through C<chromaterm> for syntax highlighting.  Note: you must use appropriate command separators/terminators to keep the meaning of the command pipline (such as C<;> and C<|> between commands).
+
+These are not put through macro parsing.});
     output '=item comms = ' . $self->parent->config->{comms};
     output $self->loc(
         'Sets the default communication method (initially taken from the name of the program, but can be overridden here).'

t/15config.t
@@ -102,6 +102,8 @@ Readonly::Hash my %default_config => {
     history_height => 10,
 
     command             => q{},
+    command_pre         => q{},
+    command_post        => q{},
     title               => q{15CONFIG.T},
     comms               => q{ssh},
     hide_menu           => 0,
@@ -564,6 +566,8 @@ auto_close=5
 auto_quit=yes
 auto_wm_decoration_offsets=no
 cols=-1
+command_post=
+command_pre=
 console=console
 console_args=
 console_position=

commit bf6e9d06486b4c8f30d87a2b463992c4b892711e

helper: Fix missing space separator for ssh_args
Docs suggest using ssh_args without space at the end
ssh_args = "-x -o ConnectTimeout=10"

+ remove also trailing space in another line.

Fixes: 82f8845 ("Add in 'command_pre' and 'command_post' configs")

Signed-off-by: Petr Vorel <petr.vorel@gmail.com>

lib/App/ClusterSSH/Helper.pm
@@ -71,9 +71,9 @@ sub script {
            my \$user=shift;
            my \$port=shift;
            my \$mstr=shift;
-           my \$command="$command_pre $comms $comms_args";
+           my \$command="$command_pre $comms $comms_args ";
            open(PIPE, ">", \$pipe) or die("Failed to open pipe: \$!\\n");
-           print PIPE "\$\$:\$ENV{WINDOWID}" 
+           print PIPE "\$\$:\$ENV{WINDOWID}"
                or die("Failed to write to pipe: $!\\n");
            close(PIPE) or die("Failed to close pipe: $!\\n");
            if(\$svr =~ m/==\$/)